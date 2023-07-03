2aefc59374fc928abd0be9a0e94aa514d6ffe362
masikotimo <masikotimo@gmail.com>
2023-07-03 21:03:43 +0300

    Tested application

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import { Platform, StatusBar, Image } from 'react-native';
+import { Platform, StatusBar, Image, Alert } from 'react-native';
 import AppLoading from 'expo-app-loading';
 import { Asset } from 'expo-asset';
 import { Block, GalioProvider } from 'galio-framework';
